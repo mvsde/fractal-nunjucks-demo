67f297a5c8eed6dbd6831d83e7748e17cf7f7775
Fynn Becker <fynn@hey.com>
2020-07-30 09:54:44 +0200

    test: Setup unit tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,15 @@ module.exports = {
   root: true,
   extends: [
     '@pangolinjs'
+  ],
+  overrides: [
+    {
+      files: [
+        'test/unit/**/*.spec.?(m)js'
+      ],
+      extends: [
+        'plugin:ava/recommended'
+      ]
+    }
   ]
 }
